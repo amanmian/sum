266c116b9dabbed360cc463efb922848f9d7883a
amanmian <amanmian.ksp@gmail.com>
2017-09-28 09:52:30 +0530

    sum

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p2
+{
+    class Program
+    {
+        
+       // public int AddNumbers(int fn, int sn)
+        public static void Main()
+        {
+            int fn, sn; 
+            Console.Write("ENTER FIRST NUMBER ");
+            fn = Convert.ToInt32(Console.ReadLine());
+            Console.Write("ENTER SECOND NUMBER ");
+            sn = Convert.ToInt32(Console.ReadLine());
+            int sum = fn + sn;
+            Console.WriteLine("SUM = " + sum);
+           // Console.ReadLine();
+            Console.ReadKey();
+            
+        }
+    }
+    
+    
+}
